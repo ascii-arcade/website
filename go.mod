bd27b40bf18d84fd9ba602699cc3cee229f08fc5
koo04 <douglas@devicariis.org>
2025-05-31 12:37:15 -0400

    Markdown to html conversion

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ascii-arcade/website
 
 go 1.23.5
+
+require github.com/russross/blackfriday/v2 v2.1.0
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"strings"
+
+	"github.com/russross/blackfriday/v2"
 )
 
 func main() {
@@ -31,9 +33,34 @@ func main() {
 		sb.WriteString("\n\n## Available Games:\n")
 		sb.WriteString("- Farkle: `ssh ascii-arcade.games -p 2022`\n")
 
-		w.Header().Set("Content-Type", "text/plain")
-		w.Write([]byte(sb.String()))
+		if isTerminal(r) {
+			w.Header().Set("Content-Type", "text/plain")
+			w.Write([]byte(sb.String()))
+			return
+		}
+
+		// convert markdown to HTML
+		htmlFlags := blackfriday.UseXHTML | blackfriday.CompletePage | blackfriday.Smartypants | blackfriday.SmartypantsFractions | blackfriday.SmartypantsDashes | blackfriday.SmartypantsLatexDashes
+		renderer := blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{
+			Flags: htmlFlags,
+			Title: "ASCII Arcade",
+			CSS:   "https://gist.githubusercontent.com/koo04/16ef1dd45e9db08de7e15c94edee2aa3/raw/2e0fa0cfaed99ddef65de1386efe8d1f6b80753e/ascii-games.css",
+		})
+		output := blackfriday.Run([]byte(sb.String()), blackfriday.WithRenderer(renderer))
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(output)
 	})
 
 	http.ListenAndServe(":81", mux)
+}
+
+func isTerminal(r *http.Request) bool {
+	terminals := []string{"curl", "wget", "lynx", "links", "elinks", "httpie"}
+	userAgent := r.Header.Get("User-Agent")
+	for _, terminal := range terminals {
+		if strings.Contains(userAgent, terminal) {
+			return true
+		}
+	}
+	return false
 }
